b731da1a27bf26d995d13e6dc43880ab541f6fe2
Frederik Van Voorden <rick@northbronson.com>
2021-06-04 22:44:01 -0700

    Initial Commit

---
+++ NBARPhotos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBARPhotos
+//
+//  Created by Rick Van Voorden on 6/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NBARPhotos/NBARPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  NBARPhotosApp.swift
+//  NBARPhotos
+//
+//  Created by Rick Van Voorden on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct NBARPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
